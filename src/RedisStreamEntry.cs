d11f20841c28f73a097cbba9a895b9122cfa4b62
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2025-08-08 23:51:52 +0300

    Create a stream type

---
+++ src/RedisStreamEntry.cs
@@ -0,0 +1,6 @@
+﻿namespace codecrafters_redis.src;
+public class RedisStreamEntry
+{
+    public string Id { get; set; }
+    public Dictionary<string , string> KeyValuePairs { get; set; } = new();
+}
