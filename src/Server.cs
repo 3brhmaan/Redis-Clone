430f9be6c71e714b8fd41e19c01bac7ce3bc15d3
abdullrhman elsayed <3brhmaaan@gmail.com>
2025-08-18 16:43:54 +0300

    fix: use dynamic port assignment from command line arguments in Redis server initialization

--- src/Server.cs
+++ src/Server.cs
@@ -2,10 +2,15 @@ using codecrafters_redis.src;
 using codecrafters_redis.src.Data.Storage;
 using codecrafters_redis.src.Locking;
 
+int port = 6379;
+if(args.Length > 0)
+{
+    port = int.Parse(args[1]);
+}
 
 var redisServer = new RedisServer(
     new RedisCommandHandler(new RedisStorage() , new KeyLockManager()) , 
-    6379
+    port
 );
 
 redisServer.Start();
