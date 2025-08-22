c1cc6175348e534e9ac9d0c1378055bd77687644
abdullrhman elsayed <3brhmaaan@gmail.com>
2025-08-22 06:08:41 +0300

    after establishing handshake between the slave and the master send Empty RDB file

--- src/RedisServer.cs
+++ src/RedisServer.cs
@@ -23,34 +23,7 @@ public class RedisServer
     {
         if (configuration.ReplicationMode == ReplicationMode.Slave)
         {
-            var master = new TcpClient(
-                configuration.MasterHost , configuration.MasterPort.Value
-            );
-
-            var stream = master.GetStream();
-
-            //PING command
-            string pingCommand = "*1\r\n$4\r\nPING\r\n";
-
-            if (SendAndVerify(stream, pingCommand, "PONG"))
-            {
-                // REPLCONF command (listening-port)
-                string replicaPort = configuration.Port.ToString();
-                string replconf1 = $"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${replicaPort.Length}\r\n{replicaPort}\r\n";
-
-                if (SendAndVerify(stream, replconf1, "OK"))
-                {
-                    // REPLCONF command (capabilities)
-                    string replconf2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
-                    
-                    // PSYNC Command with <master replication id> and <offset>
-                    string psyncCommand = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
-                    if (SendAndVerify(stream, replconf2, "OK"))
-                    {
-                        SendAndVerify(stream, psyncCommand, "FULLRESYNC");
-                    }
-                }
-            }
+            EstablishReplicationHandshake();
         }
 
         server.Start();
@@ -82,6 +55,12 @@ public class RedisServer
                 string response = commandHandler.ParseRedisCommand(request , clientId);
 
                 client.Send(Encoding.UTF8.GetBytes(response));
+
+                if (request.Contains("PSYNC"))
+                {
+                    // send empty RDB after the 3 handshake response have been sended
+                    SendEmptyRDBFile(client);
+                }
             }
 
             client.Close();
@@ -89,6 +68,50 @@ public class RedisServer
         catch (Exception ex)
         {
             Console.WriteLine(ex);
+        }
+    }
+    private void SendEmptyRDBFile(Socket client)
+    {
+        string base64RDB = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+
+        byte[] rdbBytes = Convert.FromBase64String(base64RDB);
+
+        string header = $"${rdbBytes.Length}\r\n";
+
+        client.Send(Encoding.UTF8.GetBytes(header));
+
+        client.Send(rdbBytes);
+    }
+    private void EstablishReplicationHandshake()
+    {
+        // 3 handshake between slave and master
+        var master = new TcpClient(
+            configuration.MasterHost , configuration.MasterPort.Value
+        );
+
+        var stream = master.GetStream();
+
+        //PING command
+        string pingCommand = "*1\r\n$4\r\nPING\r\n";
+
+        if (SendAndVerify(stream , pingCommand , "PONG"))
+        {
+            // REPLCONF command (listening-port)
+            string replicaPort = configuration.Port.ToString();
+            string replconf1 = $"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n${replicaPort.Length}\r\n{replicaPort}\r\n";
+
+            if (SendAndVerify(stream , replconf1 , "OK"))
+            {
+                // REPLCONF command (capabilities)
+                string replconf2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+
+                // PSYNC Command with <master replication id> and <offset>
+                string psyncCommand = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
+                if (SendAndVerify(stream , replconf2 , "OK"))
+                {
+                    SendAndVerify(stream , psyncCommand , "FULLRESYNC");
+                }
+            }
         }
     }
     private bool SendAndVerify(NetworkStream stream , string command , string expected)
