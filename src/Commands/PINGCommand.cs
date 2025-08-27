2fffc257bc7e7ae13ceea709cc0fa2226f209309
abdullrhman elsayed <3brhmaaan@gmail.com>
2025-08-27 19:02:14 +0300

    update "PING" command to repond with different response in Subscription Mode

--- src/Commands/PINGCommand.cs
+++ src/Commands/PINGCommand.cs
@@ -6,11 +6,19 @@ namespace codecrafters_redis.src.Commands;
 public class PINGCommand : RedisCommand
 {
     public override string Name => "PING";
-    public PINGCommand(IServerContext serverContext) 
+    public PINGCommand(IServerContext serverContext)
         : base(serverContext) { }
 
     public override string Execute(string[] arguments)
     {
-        return "+PONG\r\n";
+        if (_serverContext.SubscriptionManager.IsInSubscribeMode)
+        {
+            return "*2\r\n$4\r\npong\r\n$0\r\n\r\n";
+        }
+        else
+        {
+            return "+PONG\r\n";
+
+        }
     }
 }
