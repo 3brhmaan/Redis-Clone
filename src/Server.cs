bc5a233b43ee529d79d31decb01659dd4e1f76af
abdullrhman elsayed <3brhmaaan@gmail.com>
2025-08-28 01:32:03 +0300

    support "ZCARD" Command witch return the count of members of a specific key

--- src/Server.cs
+++ src/Server.cs
@@ -38,6 +38,7 @@ void RegisterCommands(IServerContext context)
     context.CommandContainer.Register(() => new ZADDCommand(context));
     context.CommandContainer.Register(() => new ZRANKCommand(context));
     context.CommandContainer.Register(() => new ZRANGECommand(context));
+    context.CommandContainer.Register(() => new ZCARDCommand(context));
 }
 
 
